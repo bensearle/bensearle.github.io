4ba7ae41edb5666e2e0db3091f731eb53e2e7b2d
Ben Searle <bensearle93@hotmail.co.uk>
2016-12-07 19:23:58 +0800

    use strict

--- geocoder/geocoder.js
+++ geocoder/geocoder.js
@@ -1,3 +1,6 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
 var input = document.getElementById("input");
 var outputJSON = document.getElementById("json");
 var outputTOML = document.getElementById("toml");
@@ -7,6 +10,7 @@ var map,
     geocoder;
 var output = [];// 
 function initializeMap() {
+    'use strict';
 	geocoder = new google.maps.Geocoder();
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 22.281896, lng: 114.155153},
@@ -18,29 +22,31 @@ function initializeMap() {
 
 //var geocoder = new google.maps.Geocoder();
 
-function readInput(){
+function readInput() {
+    'use strict';
     output = []; // clear output
     outputJSON.value = '';
     outputTOML.value = '';
     outputYAML.value = '';
 
     var lines = input.value.split('\n');
-    lines.forEach(function(line){
-        if(line.trim().length == 0){
+    lines.forEach(function (line) {
+        if (line.trim().length === 0) {
             // whitespace
         } else {
-            console.log("read line:",line);
+            console.log("read line:", line);
             var coordinates = readCoordinates(line); // return valid coordinates or false
-            if(coordinates){
+            if (coordinates) {
                 getAddress(coordinates); // get address from coordinates
             } else {
-               getCoordinates(line); // get coordinates from address
-            }   
+                getCoordinates(line); // get coordinates from address
+            }
         }
     });
 }
 
 function process() {
+    'use strict';
 	//var branchId = document.getElementById('branchId').value;
 	//console.log(branchId);
 	//var a = coordStringToArray("{'lat=1.555,'lang':1");
@@ -59,6 +65,7 @@ function process() {
  * longitude range is -180 to +180 (west and east of the Prime Meridian)
  */
 function validCoodinates(lat, lng) {
+    'use strict';
 	return -90 <= lat && lat <= 90 && -180 <= lng && lng <= 180;
 }
 
@@ -70,14 +77,15 @@ function validCoodinates(lat, lng) {
  * return {lat: 22.281896, lng: 114.155153}
  */
 function readCoordinates(coordinateString) {
+    'use strict';
 	var coordArray = coordinateString.match(/[+-]?\d+(\.\d+)/g); // matches decimal numbers (must contain ".")
-    if (coordArray){
+    if (coordArray) {
         
     
-        if (coordArray.length == 2){ // exactly 2 coordinates
+        if (coordArray.length === 2) { // exactly 2 coordinates
             var lat = parseFloat(coordArray[0]),
                 lng = parseFloat(coordArray[1]);
-            if(validCoodinates(lat, lng)){ // coordinates are valid
+            if (validCoodinates(lat, lng)) { // coordinates are valid
                 return {lat: lat, lng: lng}; // return JSON of coordinates
             }
         }
@@ -85,26 +93,27 @@ function readCoordinates(coordinateString) {
 	return false; // return false when there are not exactly 2 coordinates, both valid
 }
 
-function getAddress(coodinates,id){
+function getAddress(coodinates, id) {
+    'use strict';
 	var firstLine = true; // first line of geocode returned address
-	geocoder.geocode({'location': coodinates}, function(results, status) {
+	geocoder.geocode({'location': coodinates}, function (results, status) {
 		if (status === 'OK') {
 			if (results[1]) {
-				console.log("",results[1]);
+				console.log("", results[1]);
 				var address = results[1].formatted_address.replace(/[;]/g, ","),
                     fullAddress = "";
-				results[1].address_components.forEach(function(addressLine){
-					if (firstLine){
+				results[1].address_components.forEach(function (addressLine) {
+					if (firstLine) {
 						fullAddress += addressLine.long_name;
 						firstLine = false;
 					} else {
 						fullAddress += ", " + addressLine.long_name;
 					}
 				});
-				fullAddress = fullAddress.replace(/[;]/g,",");
+				fullAddress = fullAddress.replace(/[;]/g, ",");
 				console.log("", address);
 				console.log("", fullAddress);
-                recieveData(fullAddress,coodinates);
+                recieveData(fullAddress, coodinates);
 
 				/*
 				map.setZoom(11);
@@ -125,11 +134,12 @@ function getAddress(coodinates,id){
 }
 
 function getCoordinates(address, id) {
+    'use strict';
 	var addressString =  address.replace(/[^A-Z0-9]+/ig, " "); // regex ^:not +:match-multiple i:case-insensitive g:global-match
 
 	geocoder.geocode({'address': address}, function (results, status) {
 		if (status === 'OK') {
-            if (results[0]){
+            if (results[0]) {
                 console.log(results[0].geometry.location);
                 var location = results[0].geometry.location;
                 var coordinates = {lat: location.lat(), lng: location.lng()};
@@ -141,8 +151,9 @@ function getCoordinates(address, id) {
 	});
 }
 
-function recieveData(address,coordinates){
-    console.log("***RESULTS***",address,coordinates);
+function recieveData(address, coordinates) {
+    'use strict';
+    console.log("***RESULTS***", address, coordinates);
     
     var marker = new google.maps.Marker({
 		map: map,
@@ -150,8 +161,8 @@ function recieveData(address,coordinates){
 		zIndex: 1, // 100000 - index;
 		icon: {
 			url: 'markerOG.png',
-			labelOrigin: new google.maps.Point(20,15),
-			scaledSize: new google.maps.Size(40,40)},
+			labelOrigin: new google.maps.Point(20, 15),
+			scaledSize: new google.maps.Size(40, 40)},
 		/*label: {
 			text: ""+store.index,
 			color: 'black',
@@ -160,11 +171,11 @@ function recieveData(address,coordinates){
         //TODO extend map bounds
 	});
 
-    output.push({"address":address,"coordinates":coordinates});
+    output.push({"address": address, "coordinates": coordinates});
     console.log(JSON.stringify(output));
     
     outputJSON.value = JSON.stringify(output);
-    outputTOML.value += coordinates.lat + "," + coordinates.lng + "," + address.replace(/[,]/g, ";") + "\n"
+    outputTOML.value += coordinates.lat + "," + coordinates.lng + "," + address.replace(/[,]/g, ";") + "\n";
 }
 
 //TODO implement copy to clipboard for output http://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
