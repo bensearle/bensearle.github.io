0af4d0ff9988a0862a7dc0a627a5c0d2ac192ae0
bensearle <bensearle93@hotmail.co.uk>
2016-12-09 01:19:39 +0800

    Add bases

--- numberConvertor/numberConvertor.js
+++ numberConvertor/numberConvertor.js
@@ -22,6 +22,16 @@ var binary, // base 2
     base20 = document.getElementById('base20'),
     base24 = document.getElementById('base24'),
     base26 = document.getElementById('base26'),
+    base27 = document.getElementById('base27'),
+    base30 = document.getElementById('base30'),
+    base32 = document.getElementById('base32'),
+    base36 = document.getElementById('base36'),
+    base52 = document.getElementById('base52'),
+    base56 = document.getElementById('base56'),
+    base57 = document.getElementById('base57'),
+    base58 = document.getElementById('base58'),
+    base60 = document.getElementById('base60'),
+
     roman = document.getElementById('roman');
 
 
@@ -29,14 +39,23 @@ var binary, // base 2
 // chinese numerals
 
 //wikipedia.org/wiki/List_of_numeral_systems
+// base27 checksum
 
-var base2set = ['0', '1'],
-    base8set = ['0', '1', '2', '3', '4', '5', '6', '7'],
-    base10set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    base16set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
-    baseSet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
-    //base13set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '&#8586', '&#8587'], // rotated2, rotated3
-    base12set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\u218a', '\u218b'], // rotated2, rotated3
+var baseSet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'],
+    base12set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\u218a', '\u218b'], // rotated2, rotated3, The Dozenal Society of Great Britain notation
+    base30set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'], // Natural Area Code (NAC) notation
+    base32set = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7'], // defined in RFC 4648
+    
+    
+    //TODO change base 64 alphabet and variations https://tools.ietf.org/html/rfc4648
+    
+    base52set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'], // base62 without vowels
+    base56set = ['2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], // base58 without 1, o
+    base57set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', '+', '/'], // base62 excluding I, O, l, U, u
+    base58set = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], // base64 without similar looking letters to avoid ambiguity when reading/printing: 0, O, I, l, +, / 
+    base60set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_'], // newBase60: base62, excluding I, O, l, including _
+    base61set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', " "], // newBase61: newBase60, with a space
+
 
     romanSet = { // line on top ( ̄ ) means *1000
         1: 'I',
@@ -80,11 +99,27 @@ function covertToRoman(number) {
 function convertFromBase10(number, base) {
     "use strict";
     var digitSet = baseSet; // the set that the digits belong to
+    //TODO change to switch
     if (base === 12) {
         digitSet = base12set;
+    } else if (base === 30) {
+        digitSet = base30set;
+    } else if (base === 32) {
+        digitSet = base32set;
+    } else if (base === 52) {
+        digitSet = base52set;
+    } else if (base === 56) {
+        digitSet = base56set;
+    } else if (base === 57) {
+        digitSet = base57set;
+    } else if (base === 58) {
+        digitSet = base58set;
     }
+    
     var r = number % base; // get the mod/remainder
-    if (r === number) {
+    if (number === 0) {
+        return "";
+    } else if (r === number) {
         return digitSet[r];
     } else {
         return convertFromBase10((number - r) / base, base) + digitSet[r];
@@ -100,6 +135,18 @@ function convertToBase10(number, base) {
         i = digits.length - 1;
     if (number === 12) {
         digitSet = base12set;
+    } else if (base === 30) {
+        digitSet = base30set;
+    } else if (base === 32) {
+        digitSet = base32set;
+    } else if (base === 52) {
+        digitSet = base52set;
+    } else if (base === 56) {
+        digitSet = base56set;
+    } else if (base === 57) {
+        digitSet = base57set;
+    } else if (base === 58) {
+        digitSet = base58set;
     }
     
     digits.forEach(function (digit) {
@@ -126,9 +173,26 @@ function recalculate() {
     base14.value = convertFromBase10(base10number, 14);
     base15.value = convertFromBase10(base10number, 15);
     base16.value = convertFromBase10(base10number, 16);
-    //base20.value = convertFromBase10(base10number, 20);
-    //base24.value = convertFromBase10(base10number, 24);
-    //base26.value = convertFromBase10(base10number, 26);
+    base20.value = convertFromBase10(base10number, 20);
+    base24.value = convertFromBase10(base10number, 24);
+    base26.value = convertFromBase10(base10number, 26);
+
+    base27.value = convertFromBase10(base10number, 27);
+    base30.value = convertFromBase10(base10number, 30);
+    base32.value = convertFromBase10(base10number, 32);
+    base36.value = convertFromBase10(base10number, 36);
+    base52.value = convertFromBase10(base10number, 52);
+    base56.value = convertFromBase10(base10number, 56);
+    base57.value = convertFromBase10(base10number, 57);
+    base58.value = convertFromBase10(base10number, 58);
+    base60.value = convertFromBase10(base10number, 60);
+
+    
+    
+    
+    
+    
+    
     roman.value = covertToRoman(base10number, 16);
 
     console.log(convertFromBase10(base10number, 13));
