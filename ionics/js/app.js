df00a3d920f79013b5b0624a729a4466332bb686
Ben Searle <bensearle93@hotmail.co.uk>
2017-01-13 16:29:09 +0800

    ionics

---
+++ ionics/js/app.js
@@ -0,0 +1,147 @@
+/*global angular*/
+
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+	.run(function ($ionicPlatform) {
+		$ionicPlatform.ready(function () {
+			if (window.cordova && window.cordova.plugins.Keyboard) {
+				// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+				// for form inputs)
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+				// Don't remove this line unless you know what you are doing. It stops the viewport
+				// from snapping when text inputs are focused. Ionic handles this internally for
+				// a much nicer keyboard experience.
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+			if (window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+		});
+	});
+
+angular.module('todo', ['ionic'])
+
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "home.html"
+            })
+            .state('customerOrders', {
+                url: "/customerOrders",
+                templateUrl: "customerOrders.html" 
+            });
+        $urlRouterProvider.otherwise("/home");
+      })
+
+    .controller('TodoCtrl', function ($scope, $ionicModal) {
+        // test data
+        $scope.tasks = [
+            { title: 'Collect coins' },
+            { title: 'Eat mushrooms' },
+            { title: 'Get high enough to grab the flag' },
+            { title: 'Find the Princess' }
+        ];
+    
+        // populate customer orders
+        $scope.customerOrders = userData.orders;
+    
+        // Open our new task modal
+        $scope.viewOrder = function (order) {
+            $scope.selectedOrder = order;
+            console.log(order);
+            
+            /*$scope.groups = {};
+            for (var i = 0; i < order.items.length; i += 1){
+                $scope.groups[order.items[i].currentBranch] = order.items[i];
+            }*/
+
+
+            var groups = {};
+            for (var i = 0; i < order.items.length; i++) {
+                var groupName = order.items[i].currentBranch;
+                if (!groups[groupName]) {
+                    groups[groupName] = [];
+                }
+                groups[groupName].push(order.items[i]);
+            }
+            myArray = [];
+            for (var groupName in groups) {
+                myArray.push({'name': groupName, 
+                    'title': groups[groupName].length + ' Item(s) from ' + groupName, 
+                    'items': groups[groupName], 
+                    'show': true});
+            }
+            console.log(myArray);
+            console.log(order.items);
+            
+            $scope.groups = myArray;
+            /*$scope.groups[0] = {
+                    name: {left: "Order Details", right: "Status"},
+                    items: [],
+                    show: false
+            };
+            for (var i=1; i<10; i++) {
+                $scope.groups[i] = {
+                    name: {left: "...Item(s) from...", right: "Ticket Number"},
+                    items: [],
+                    show: false
+                };
+                for (var j=0; j<3; j++) {
+                    $scope.groups[i].items.push(i + '-' + j);
+                }
+            };*/
+            
+            console.log($scope.groups);
+            
+        };
+
+       
+    
+        $scope.toggleGroup = function (group) {
+            group.show = !group.show;
+        };
+        $scope.isGroupShown = function (group) {
+            return group.show;
+        };
+
+        $scope.isSelectedOrder = function (order) {
+            return order.orderNumber === $scope.selectedOrder.orderNumber;
+        };
+
+        // Create and load the Modal
+        $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+        // Called when the form is submitted
+        $scope.createTask = function (task) {
+            $scope.tasks.push({
+                title: task.title
+            });
+            $scope.taskModal.hide();
+            task.title = "";
+        };
+
+        // Open our new task modal
+        $scope.newTask = function () {
+            $scope.taskModal.show();
+        };
+
+        // Close the new task modal
+        $scope.closeNewTask = function () {
+            $scope.taskModal.hide();
+        };
+
+    })
+
+;
---
+++ ionics/userData.js
@@ -0,0 +1,262 @@
+var userData=
+{
+  "orders": [
+    {
+      "collectionDate": "",
+      "totalPrice": "HK$567",
+      "itemsCount": "3",
+      "items": [
+        {
+            "itemTitle":"Argyle Shoe",
+            "currentBranch": "Ausie",
+            "barcode": "SKU#100",
+            "itemStatus": "Pending Pick",
+            "reservationId": 50510,
+            "shipmentNumber": "",
+            "size": "SIZE 1",
+            "styleRef": "SKU10001",
+            "tickNumber": "718",
+            "title": ""
+        },
+        {
+            "itemTitle":"Trousers",
+            "currentBranch": "Ausie",
+            "barcode": "SKU#100",
+            "itemStatus": "Pending Pick",
+            "reservationId": 50510,
+            "shipmentNumber": "",
+            "size": "SIZE 1",
+            "styleRef": "SKU10001",
+            "tickNumber": "719",
+            "title": ""
+        },
+        {
+            "itemTitle":"T-Shirt",
+            "currentBranch": "Warehouse",
+            "barcode": "SKU#200",
+            "itemStatus": "Pending Pick",
+            "reservationId": 50510,
+            "shipmentNumber": "",
+            "size": "1",
+            "styleRef": "SKU10001",
+            "tickNumber": "720",
+            "title": ""
+        }
+      ],
+      "createDate": "2016-12-06 08:55:09",
+      "currency": "HKD",
+      "customerFeedback": 1,
+      "customerName": "John abc",
+      "datePlace": "06/12/2016",
+      "details": [
+        {
+          
+        }
+      ],
+      "displayPrice": "HK$100",
+      "extendDays": 2,
+      "isOverdue": 0,
+      "mobileNumber": "61431002293",
+      "modifyConfirmDate": false,
+      "orderId": 21556,
+      "orderNumber": "print90",
+      "orderStatus": "2",
+      "originBranch": "WebReserve",
+      "payBarcode": "tx12134345",
+      "shippingRelease": "Hold",
+      "statusTitle": "Stock Reserved",
+      "type": 3,
+      "values": 100
+    },
+    {
+      "itemTitle":"Starry Tee",
+      "collectionDate": "",
+      "commentHistory": [],
+      "createDate": "2016-12-07 02:11:10",
+      "currency": "HKD",
+      "currentBranch": "Ausie",
+      "customerFeedback": 1,
+      "customerName": "Brenda Test",
+      "datePlace": "07/12/2016",
+      "details": [
+        {
+          "appointmentTimeArray": [],
+          "barcode": "SKU#100",
+          "historys": [
+            {
+              "failedReason": "Not Found",
+              "pickDate": "07/12/2016",
+              "pickStatus": 7,
+              "pickStore": "Ausie"
+            }
+          ],
+          "itemStatus": "Pending Pick",
+          "reservationId": 50545,
+          "shipmentNumber": "",
+          "size": "1",
+          "styleRef": "SKU10001",
+          "tickNumber": "",
+          "title": ""
+        }
+      ],
+      "displayPrice": "HK$1000",
+      "extendDays": 2,
+      "isOverdue": 0,
+      "items": 1,
+      "mobileNumber": "61431002293",
+      "modifyConfirmDate": false,
+      "orderId": 21588,
+      "orderNumber": "mayday2",
+      "orderStatus": "1",
+      "originBranch": "WebReserve",
+      "payBarcode": "tx12134345",
+      "shippingRelease": "Hold",
+      "statusTitle": "Insufficient Stock",
+      "type": 2,
+      "values": 100
+    },
+    {
+      "itemTitle":"Floral Tee",
+      "collectionDate": "",
+      "commentHistory": [],
+      "createDate": "2016-12-06 01:30:52",
+      "currency": "HKD",
+      "currentBranch": "Ausie",
+      "customerFeedback": 1,
+      "customerName": "ABC Test",
+      "datePlace": "06/12/2016",
+      "details": [
+        {
+          "appointmentTimeArray": [],
+          "barcode": "SKU#100",
+          "historys": [
+            {
+              "failedReason": "Not Found",
+              "pickDate": "06/12/2016",
+              "pickStatus": 7,
+              "pickStore": "Ausie"
+            }
+          ],
+          "itemStatus": "Pending Pick",
+          "reservationId": 50545,
+          "shipmentNumber": "",
+          "size": "1",
+          "styleRef": "SKU10001",
+          "tickNumber": "",
+          "title": ""
+        }
+      ],
+      "displayPrice": "HK$100",
+      "extendDays": 2,
+      "isOverdue": 0,
+      "items": 1,
+      "mobileNumber": "61431002293",
+      "modifyConfirmDate": false,
+      "orderId": 21588,
+      "orderNumber": "mayday3",
+      "orderStatus": "1",
+      "originBranch": "WebReserve",
+      "payBarcode": "tx12134345",
+      "shippingRelease": "Hold",
+      "statusTitle": "Insufficient Stock",
+      "type": 2,
+      "values": 100
+    },
+    {
+      "itemTitle":"Floral Bag",
+      "collectionDate": "",
+      "commentHistory": [],
+      "createDate": "2016-12-05 13:33:09",
+      "currency": "HKD",
+      "currentBranch": "Ausie",
+      "customerFeedback": 1,
+      "customerName": "Hello Test",
+      "datePlace": "05/12/2016",
+      "details": [
+        {
+          "appointmentTimeArray": [],
+          "barcode": "SKU#100",
+          "historys": [
+            {
+              "failedReason": "Not Found",
+              "pickDate": "06/12/2016",
+              "pickStatus": 7,
+              "pickStore": "Ausie"
+            }
+          ],
+          "itemStatus": "Pending Pick",
+          "reservationId": 50545,
+          "shipmentNumber": "",
+          "size": "1",
+          "styleRef": "SKU10001",
+          "tickNumber": "",
+          "title": ""
+        }
+      ],
+      "displayPrice": "HK$100",
+      "extendDays": 2,
+      "isOverdue": 0,
+      "items": 1,
+      "mobileNumber": "61431002293",
+      "modifyConfirmDate": false,
+      "orderId": 21588,
+      "orderNumber": "mayday4",
+      "orderStatus": "1",
+      "originBranch": "WebReserve",
+      "payBarcode": "tx12134345",
+      "shippingRelease": "Hold",
+      "statusTitle": "Insufficient Stock",
+      "type": 2,
+      "values": 100
+    },
+    {
+      "itemTitle":"Graphic Bag",
+      "collectionDate": "",
+      "commentHistory": [],
+      "createDate": "2016-12-05 10:11:11",
+      "currency": "HKD",
+      "currentBranch": "Ausie",
+      "customerFeedback": 1,
+      "customerName": "Testing Test",
+      "datePlace": "05/12/2016",
+      "details": [
+        {
+          "appointmentTimeArray": [],
+          "barcode": "SKU#100",
+          "historys": [
+            {
+              "failedReason": "Not Found",
+              "pickDate": "05/12/2016",
+              "pickStatus": 7,
+              "pickStore": "Ausie"
+            }
+          ],
+          "itemStatus": "Pending Pick",
+          "reservationId": 50545,
+          "shipmentNumber": "",
+          "size": "1",
+          "styleRef": "SKU10001",
+          "tickNumber": "",
+          "title": ""
+        }
+      ],
+      "displayPrice": "HK$100",
+      "extendDays": 2,
+      "isOverdue": 0,
+      "items": 1,
+      "mobileNumber": "61431002293",
+      "modifyConfirmDate": false,
+      "orderId": 21588,
+      "orderNumber": "mayday6",
+      "orderStatus": "1",
+      "originBranch": "WebReserve",
+      "payBarcode": "tx12134345",
+      "shippingRelease": "Hold",
+      "statusTitle": "Insufficient Stock",
+      "type": 2,
+      "values": 100
+    }
+  ],
+  "resultCode": 0,
+  "statusMsg": "succ"
+};
